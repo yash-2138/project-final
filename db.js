770f73e83450f3bfeeaf0a1b9dcb106579c47ca2
Kunalkl <113526650+Kunalkl@users.noreply.github.com>
2024-01-25 14:43:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ const mysql = require('mysql2')
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: 'root',
-=======
-    password: '1234567890',
->>>>>>> bf5155cb6ac8578d9868c130bf1270d93b6cf99a
     database: 'decentStorage',
 });
   db.getConnection((err, connection) => {
